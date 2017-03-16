f63854ab688e5c43271438d3c5f71e154bdba613
Ignace Bleukx <ignace.bleukx@student.kuleuven.be>
2017-03-16 10:50:19 +0100

    Class World uitgebreid met getters en setters

--- src/asteroids/model/World.java
+++ src/asteroids/model/World.java
@@ -3,6 +3,21 @@ package asteroids.model;
 public class World {
 
 	public World (double width, double height){
-		this.setDimensions(width, height);
+		this.setDimension(width, height);
 	}
+	
+	public void setDimension(double width, double height){
+		this.width = width;
+		this.height = height;
+	}
+	
+	public double[] getDimension(){
+		return new double[]{this.width, this.height};
+	}
+	
+	public boolean isValidDimension(double width, double height){
+		return true;
+	}
+	private double width;
+	private double height;
 }
