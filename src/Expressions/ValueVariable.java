d10e8bd5cc11961c7cc2e28c474691ea5ec56ec6
Ignace Bleukx <ignace.bleukx@student.kuleuven.be>
2017-05-24 12:49:44 +0200

    Nog enkele errors in programs gefixt

---
+++ src/Expressions/ValueVariable.java
@@ -0,0 +1,39 @@
+package Expressions;
+
+import asteroids.util.ModelException;
+
+public class ValueVariable extends ValueExpression {
+
+	public ValueVariable (Expression expression, String name){
+		setName(name);
+		setExpression(expression);
+	}
+	
+	public Expression getExpression() {
+		return expression;
+	}
+	public void setExpression(Expression expression) {
+		this.expression = expression;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	private Expression expression;
+	private String Name;
+	
+	public double evaluate() throws ModelException{
+		if (getExpression() instanceof ValueExpression){
+			passInformation(getExpression());
+			return ((ValueExpression) getExpression()).evaluate();
+		}
+		else{
+			throw new ModelException("The ValueVariable expression does not evaluate to a value");
+		}
+	}
+}
