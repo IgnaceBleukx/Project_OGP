243c8a90c492348aa51cbc6535b93c463f3a3d79
Ignace Bleukx <ignace.bleukx@student.kuleuven.be>
2017-05-25 15:03:16 +0200

    Documentatie world afgewerkt

--- src/asteroids/model/World.java
+++ src/asteroids/model/World.java
@@ -129,7 +129,6 @@ public class World {
 	 * 			| new.getWorld() = null
 	 */
 	public void removeShipFromWorld(Ship ship) throws IllegalArgumentException{
-		
 		if (ship != null && this.getAllShips().contains(ship)){
 			this.getAllShips().remove(ship);
 			ship.setWorld(null);
@@ -139,6 +138,8 @@ public class World {
 		}
 		
 	}
+	
+	private Set<Ship> allShips = new HashSet<Ship>();
 	
 	/**
 	 * This method returns a HashSet of all the bullets currently placed in the world.
@@ -190,7 +191,6 @@ public class World {
 		}catch (NullPointerException exc){
 			throw new IllegalArgumentException();
 		}	}
-
 	
 	/**
 	 * This method removes the bullet from the current world.
@@ -210,7 +210,9 @@ public class World {
 			throw new IllegalArgumentException();
 		}
 	}
-	
+
+	private Set<Bullet> allBulletsWorld = new HashSet<Bullet>();
+
 	/**
 	 * This method returns a set of all entities placed in the world.
 	 * @return Returns a HashSet of all the entities in this world.
@@ -253,15 +255,39 @@ public class World {
 	
 	/**
 	 * This method adds the asteroid to the world.
+	 * @throws IllegalArgumentExcpetion: If the asteroid is null, or the asteroid is already placed in a world or
+	 * 				the asteroid is out of bounds of this world or the asteroid would overlap with another entity,
+	 * 					a new IllegalArgumentException is thrown.
 	 * @param asteroid The asteroid to be added.
 	 * @post The world contains the asteroid.
 	 * 			| new.getAllAsteroids().contains(asteroid)
 	 */
-	public void addAsteroid(Asteroid asteroid){
-		this.getAllAsteroids().add(asteroid);
-		asteroid.setWorld(this);
+	public void addAsteroid(Asteroid asteroid) throws IllegalArgumentException{
+		if (asteroid == null || asteroid.getWorld() != null || asteroid.isOutOfBounds(this)){
+			throw new IllegalArgumentException("The ship is null or out of bounds or is already in a World");
+		}
+		boolean toAddToWorld = true;
+		for(Entity entity : this.getAllEntities()){
+			if (asteroid.overlap(entity)){
+				toAddToWorld = false;
+			}
+		}
+		if(toAddToWorld){
+			this.getAllAsteroids().add(asteroid);
+			asteroid.setWorld(this);
+		}
+		else{
+			throw new IllegalArgumentException("The asteroid would overlap with another entity in the world");
+		}
 	}
 	
+	/**
+	 * This method removes the given asteroid from this world.
+	 * @param asteroid The asteroid that must be removed from this world.
+	 * @throws IllegalArgumentException: If the asteroid is not part of this world an new IllegalArgumentException is thrown.
+	 * @Post The world does not contain the asteroid given.
+	 * 			| !new.getAllAsteroids().contains(asteroid)
+	 */
 	public void removeAsteroid(Asteroid asteroid) throws IllegalArgumentException {
 		if (this.allAsteroids.contains(asteroid)){
 			allAsteroids.remove(asteroid);
@@ -271,18 +297,54 @@ public class World {
 		}
 	}
 	
+	/**
+	 * This method returns all asteroids currently placed in this world.
+	 * @return Returns a HasSet containing all asteroids in this world.
+	 * 			| if (asteroid.getWorld().equals(this)
+	 * 				result.contains(asteroid)
+	 */
 	public Set<Asteroid> getAllAsteroids(){
 		return this.allAsteroids;
 	}
 	
-	
 	private Set<Asteroid> allAsteroids = new HashSet<Asteroid>();
 
-	public void addPlanetoid(Planetoid planetoid){
-		this.allPlanetoids.add(planetoid);
-		planetoid.setWorld(this);
+	/**
+	 * @throws IllegalArgumentException: If the planetoid is null, or the planetoid is already placed in a world or
+	 * 				the planetoid is out of bounds of this world or the planetoid would overlap with another entity,
+	 * 					a new IllegalArgumentException is thrown.
+	 * @param planetoid The planetoid to be added to this world.
+	 * @Post The world contains the planetoid and the world of the planetoid is set to this.
+	 * 			| new.getAllPlanetoids().contains(planetoid).
+	 * 			| planetoid.getWorld().equals(this)
+	 */
+	public void addPlanetoid(Planetoid planetoid) throws IllegalArgumentException {
+		if (planetoid == null || planetoid.getWorld() != null || planetoid.isOutOfBounds(this)){
+			throw new IllegalArgumentException("The ship is null or out of bounds or is already in a World");
+		}
+		boolean toAddToWorld = true;
+		for(Entity entity : this.getAllEntities()){
+			if (planetoid.overlap(entity)){
+				toAddToWorld = false;
+			}
+		}
+		if(toAddToWorld){
+			this.getAllPlanetoids().add(planetoid);
+			planetoid.setWorld(this);
+		}
+		else{
+			throw new IllegalArgumentException("Ship would be overlapping another Ship in the world");
+		}
 	}
-	
+		
+	/**
+	 * This method removes the planetoid from the world.
+	 * @param planetoid The planetoid to be removed from this world.
+	 * @throws IllegalArgumentException: If the planetoid is not part of this world, a new IllegalArgumentException is thrown.
+	 * @post This world does not contain the planetoid and the world of the planetoid equals null.
+	 * 			| !new.getAllPlanetoids().contains(planetoid)
+	 * 			| planetoid.getWorld() = null
+	 */
 	public void removePlanetoid(Planetoid planetoid) throws IllegalArgumentException {
 		if (this.allPlanetoids.contains(planetoid)){
 			allPlanetoids.remove(planetoid);
@@ -292,12 +354,24 @@ public class World {
 		}
 	}
 	
+	/**
+	 * This method returns all the planetoids currently in this world.
+	 * @return A HashSet containing all planetoids in this world.
+	 * 			| if (planetoid.getWorld().equals(this)
+	 * 				result.contains(planetoid)
+	 */
 	public Set<Planetoid> getAllPlanetoids(){
 		return this.allPlanetoids;
 	}
 	
 	private Set<Planetoid> allPlanetoids = new HashSet<Planetoid>();
 	
+	/**
+	 * This method returns a set of all the MinorPlanets currently placed in this world.
+	 * @return A HashSet containing all the MinorPlanets placed in this world.
+	 * 			| if (minorPlanet.getWorld().equals(this)
+	 * 				result.contains(minorPlanet)
+	 */
 	public Set<MinorPlanet> getAllMinorPlanets(){
 		HashSet<MinorPlanet> minorPlanets = new HashSet<MinorPlanet>();
 		for (Planetoid planetoid : this.getAllPlanetoids()){
@@ -309,6 +383,12 @@ public class World {
 		return minorPlanets;
 	}	
 		
+	/**
+	 * This method returns the entities of the next collision in an array of length 2. If the collision is of the type
+	 * 	'BoundaryCollision', the second Entity in the array equals null.
+	 * @return  Returns the entities of the next collision in an array of length 2. If the collision is of the type
+	 * 				'BoundaryCollision', the second Entity in the array equals null. 
+	 */
 	public Entity[] getEntitiesNextCollision(){
 		double nextCollisionTime = Double.POSITIVE_INFINITY;
 		Entity collisionEntity1 = null;
@@ -382,7 +462,7 @@ public class World {
 					double y = entitiesNextCollision[0].getPositionCollisionBoundary()[1];
 					collisionListener.boundaryCollision(entitiesNextCollision[0], x, y);
 				}
-				this.collisionResolver(entitiesNextCollision[0]);
+				entitiesNextCollision[0].boundaryCollision();
 			}
 			else{		
 				if (collisionListener != null) {
@@ -396,6 +476,11 @@ public class World {
 		}
 	}
 
+	/**
+	 * This method determines the right type of entitycollision and calls its according methods.
+	 * @param entity1 The first Entity of the collision
+	 * @param entity2 The second Entity of the collision
+	 */
 	public void collisionResolver(Entity entity1, Entity entity2){
 		if (entity1 instanceof Ship && entity2 instanceof Ship){
 			((Ship) entity1).shipCollision((Ship) entity2);
@@ -429,35 +514,32 @@ public class World {
 			throw new IllegalStateException();
 		}
 	}
-	
-	public void collisionResolver(Entity entity){
-		if (entity instanceof Bullet){
-			((Bullet)entity).boundaryCollision();
-		}
-		else if(entity instanceof Ship || entity instanceof Asteroid) {
-			entity.boundaryCollision();
-		}
-	}
-	
-	public void terminate(){
-		while (!this.getAllBullets().isEmpty()){
-			Bullet bullet = this.getAllBullets().iterator().next();
-			bullet.terminate();
-		}
-		while (!this.getAllShips().isEmpty()){
-			Ship ship = this.getAllShips().iterator().next();
-			ship.terminate();
+
+	/**
+	 * This method terminates all its containings and itself.
+	 * @Post The world is terminated and all its containings are terminated.
+	 * 			| for (entity : this.getAllEntities)
+	 * 				entity.isTerminated()
+	 * 			| new.getAllEntities().isEmpty()
+	 * 			| new.isTerminated()
+	 */
+	public void terminate(){		
+		while(!this.getAllEntities().isEmpty()){
+			Entity entity = this.getAllEntities().iterator().next();
+			entity.terminate();
 		}
 		this.isTerminated = true;
 	}
 	
+	/**
+	 * This method returns if the world is terminated or not.
+	 * @return Return false if the world is not terminated, returs true if the world is terminated.
+	 * 			| return this.isTerminated
+	 */
 	public boolean isTerminated(){
 		return this.isTerminated;
 	}
 	
 	private boolean isTerminated = false;
-
-	private Set<Ship> allShips = new HashSet<Ship>();
-	private Set<Bullet> allBulletsWorld = new HashSet<Bullet>();
 	
 }
